288edb5039d62b307193b3c3937cdf7a5e2c1375
mirajshrestha <mirajacharya9@gmail.com>
2025-12-17 13:33:57 -0500

    Updates on the pages

--- Main.py
+++ Main.py
@@ -4,31 +4,37 @@
 
 from flask import Flask, render_template, redirect, url_for
 import sqlalchemy
-
+import flask_login
 
 app = Flask(__name__)
+
 
 # Home page
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 # About page
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route('/admin')
 def admin():
     return redirect(url_for("home"))
+
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+
 @app.route('/purchases')
 def purchase():
     return render_template("purchases.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
