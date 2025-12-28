b3e47580c750e1f2e25307c2ce4c54603dbafbaa
Miraj A. <mirajacharya9@gmail.com>
2025-12-28 12:47:50 -0500

    Login input boxes added

--- Main.py
+++ Main.py
@@ -2,11 +2,16 @@
 # Authors: Miraj Acharya, Christopher Shane Rocco Vitello
 # December 2025
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlalchemy
 import flask_login
 
 app = Flask(__name__)
+
+app.secret_key = "Test123" # Enables ude of sessions,
+# Test123 is a TEST VALUE, CHANGE LATER!!!!!!!
+
+admin = False
 
 
 # Home page
@@ -25,9 +30,19 @@ def about():
 def admin():
     return redirect(url_for("home"))
 
-
-@app.route('/login')
+# URL for login AND
+# had GET --> user opens page
+# and POST --> user submits form
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # IF user clicked the login button,
+    # take the username and password they submitted
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # UNFINISHED !!!!!!
     return render_template("login.html")
 
 
